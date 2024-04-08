507b65545760d89e6089cd3ac981e11a91fcb333
Dagim Alemu <dagimalemux@gmail.com>
2024-04-08 22:30:55 +0300

    c

---
+++ src/chapterTwo/Incrementing.java
@@ -0,0 +1,18 @@
+package chapterTwo;
+
+public class Incrementing {
+	public static void main(String[] args) {
+		int c = 5;
+	    int d = 5;
+		
+		System.out.println(c);
+		System.out.println(c++);
+		System.out.println(c);
+		System.out.println("--------------------------------");
+		
+		System.out.println(d);
+		System.out.println(++d);
+		System.out.println(d);
+	}
+
+}
