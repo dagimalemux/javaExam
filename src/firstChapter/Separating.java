433c25a535ef0ce97dbfe6c584fdce3157907c51
Dagim Alemu <dagimalemux@gmail.com>
2024-04-08 20:20:43 +0300

    separating number digits

--- src/firstChapter/Separating.java
+++ src/firstChapter/Separating.java
@@ -5,9 +5,21 @@ public class Separating {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("Enter Number :");
-		double num = sc.nextDouble();
+		System.out.println("Enter a five digit number :");
+		int num = sc.nextInt();
 		
+		if(num < 10000 || num > 99999) {
+			System.out.println("Please enter a 5 digit number.");
+		}
+		
+		else {
+			String digit = String.valueOf(num);
+			
+			for( int i=0; i < digit.length(); i++) {
+				System.out.print(digit.charAt(i)+"        ");
+			}
+		}
+		sc.close();
 		
 	}
 
